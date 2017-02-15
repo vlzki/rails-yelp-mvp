413ca2f0f9fe91c159a49f18f50864489dd0f1dd
vlzki <vlzki.dev@gmail.com>
2017-02-15 16:09:01 +0100

    restaurant CRUD finished

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,54 @@
+class RestaurantsController < ApplicationController
+
+before_action :set_restaurant, only: [:edit, :show, :update, :destroy]
+
+
+def index
+  @restaurants = Restaurant.all
+end
+
+def new
+  @restaurant = Restaurant.new
+end
+
+def create
+  @restaurant = Restaurant.new(restaurant_params)
+  @restaurant.save
+  redirect_to restaurants_path
+end
+
+def edit
+
+end
+
+def show
+
+end
+
+def update
+  @restaurant.update(restaurant_params)
+  if @restaurant.save
+    redirect_to restaurants_path
+  else
+    render :new
+  end
+end
+
+def destroy
+  @restaurant.destroy
+  redirect_to restaurants_path
+end
+
+
+
+private
+
+def set_restaurant
+  @restaurant = Restaurant.find(params[:id])
+end
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0177009887",
+    category: "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0154789722",
+    category: "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0122334455",
+    category: "japanese"
+  },
+  {
+    name:         "Le grillon",
+    address:      "place de la bourse 33000 Bordeaux",
+    phone_number: "0556034412",
+    category: "french"
+  },
+  {
+    name:         "Ajax Camp",
+    address:      "5 impasse des coders 47550 BOE",
+    phone_number: "0553007001",
+    category: "belgian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
